318268fa8de756ba9712648f0bb0998ba0f3ec48
Diogo <diogoslva0133@gmail.com>
2022-11-01 10:02:07 -0300

    Finishing simple navigation within the page - 4 new functions

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,11 +1,23 @@
-import './Content.css'
+import "./Content.css";
 import React from "react";
-
+import { Routes, Route } from "react-router-dom"; // Importe esses arquivos
+ 
+import Home from "../../views/examples/Home";
+import Param from "../../views/examples/Param";
+import About from "../../views/examples/About";
+import NotFound from "../../views/examples/NotFound";
+ 
 const Content = props => (
-    <main className="Content">
-        Componente Content
-    </main>
+  <main className="Content">
+   
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/param/:id" element={<Param />} />
+      <Route path="/" exact element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
 
-)
-
-export default Content
+  </main>
+);
+ 
+export default Content;
